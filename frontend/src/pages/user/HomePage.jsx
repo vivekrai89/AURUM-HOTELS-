39ba1878a1b4a79e84bb5c1a943d56b4905b1f67
Vivek Rai <154799453+vivekrai89@users.noreply.github.com>
2026-06-06 13:33:18 +0530

    Update HomePage.jsx

--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -301,6 +301,7 @@ export default function HomePage() {
         </div>
       </section>
 
+
       {/* Amenities */}
       <section id="amenities" className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
